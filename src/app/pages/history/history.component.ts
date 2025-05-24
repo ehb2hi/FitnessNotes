1a48e5b444fbb1de0c2db2484515e6556964d9f9
ehb2hi <brahim.elhamdaoui@outlook.com>
2025-05-24 17:11:46 +0200

    step5: implement UI

--- src/app/pages/history/history.component.ts
+++ src/app/pages/history/history.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WorkoutService } from '../../services/workout.service';
+import { WorkoutEntry } from '../../models/workout-entry.model';
 
 @Component({
   selector: 'app-history',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './history.component.html',
-  styleUrl: './history.component.scss'
+  styleUrls: ['./history.component.scss']
 })
-export class HistoryComponent {
+export class HistoryComponent implements OnInit {
+  workouts: WorkoutEntry[] = [];
 
+  constructor(private workoutService: WorkoutService) {}
+
+  ngOnInit(): void {
+    this.workouts = this.workoutService.getAllEntries();
+  }
 }
