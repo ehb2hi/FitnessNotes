d2373ed11c3f8b5efab010426d405fff36d563e0
ehb2hi <brahim.elhamdaoui@outlook.com>
2025-05-25 12:17:45 +0200

    reinstall capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fitnessnotes.app',
+  appName: 'fitness-notes',
+  webDir: 'dist/browser'
+};
+
+export default config;
