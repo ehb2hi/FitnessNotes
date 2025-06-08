7844ef1c7d7dcd41948af99c802b41bfa6fa7e84
ehb2hi <brahim.elhamdaoui@outlook.com>
2025-06-08 09:05:12 +0200

    delete the workouts component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { WorkoutsComponent } from './pages/workouts/workouts.component';
 import { HistoryComponent } from './pages/history/history.component';
 import { CategoryListComponent } from './pages/categories/category-list.component';
 import { ExerciseListComponent } from './pages/exercises/exercise-list.component';
@@ -9,7 +8,6 @@ export const routes: Routes = [
   { path: '', redirectTo: 'categories', pathMatch: 'full' },
   { path: 'categories', component: CategoryListComponent },
   { path: 'exercises/:categoryName', component: ExerciseListComponent },
-  { path: 'workouts', component: WorkoutsComponent },
   { path: 'exercise/:exerciseName', component: ExerciseFormComponent },
   { path: 'exercise-history/:exerciseName', component: HistoryComponent },
   { path: 'history', component: HistoryComponent }
