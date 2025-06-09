7409b94d5ef90f958ec7e1cbddbc49aac63a6bb1
ehb2hi <brahim.elhamdaoui@outlook.com>
2025-06-09 10:22:37 +0200

    fix the icon problem, extend 3 dots

--- src/app/shared/topbar.component.ts
+++ src/app/shared/topbar.component.ts
@@ -1,16 +1,28 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-topbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule], // <== this is correct
   templateUrl: './topbar.component.html',
   styleUrls: ['./topbar.component.scss']
 })
 export class TopbarComponent {
+  isMenuOpen = false;
 
-  
+  constructor(private router: Router) {}
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  navigateTo(path: string): void {
+    this.isMenuOpen = false;
+    this.router.navigate([path]);
+  }
+
   onMenuClick(): void {
     console.log('Overflow menu clicked!');
     // Later we can show a dropdown or Capacitor Action Sheet here
