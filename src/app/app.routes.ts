61472cba025d39687d28af77dab7bf76dd02cf17
ehb2hi <brahim.elhamdaoui@outlook.com>
2025-06-09 12:13:35 +0200

    add about page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HistoryComponent } from './pages/history/history.component';
 import { CategoryListComponent } from './pages/categories/category-list.component';
 import { ExerciseListComponent } from './pages/exercises/exercise-list.component';
 import { ExerciseFormComponent } from './pages/exercises/exercise-form.component';
+import { AboutComponent } from './pages/about/about.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'categories', pathMatch: 'full' },
@@ -10,5 +11,6 @@ export const routes: Routes = [
   { path: 'exercises/:categoryName', component: ExerciseListComponent },
   { path: 'exercise/:exerciseName', component: ExerciseFormComponent },
   { path: 'exercise-history/:exerciseName', component: HistoryComponent },
+  { path: 'about', component: AboutComponent },
   { path: 'history', component: HistoryComponent }
 ];
